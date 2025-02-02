c86389a847e3f5bac77d1277fd5a683fbefce149
Fabricio <fabriciobencomo.viloria@gmail.com>
2025-02-02 03:34:48 -0400

    chat bubbles components added

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,3 +1,5 @@
+import { AiMessages } from "../../components/chat-bubbles/AiMessages"
+import { MyMessage } from "../../components/chat-bubbles/MyMessage"
 
 export const OrthographyPage = () => {
   return (
@@ -5,6 +7,8 @@ export const OrthographyPage = () => {
       <div className="chat-messages">
         <div className="grid grid-cols-12 gap-y-2">
           {/* Bienvenido */}
+          <AiMessages text="Hola, Puedo ayudar Con tus Correciones" />
+          <MyMessage text="Hola Mundo"/>
         </div>
       </div>
     </div>
